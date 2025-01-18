15279a9b3ebc5b5449325392869c410e18b6d195
tushant-akar <tushantakar10@gmail.com>
2025-01-18 21:57:05 +0530

    Homepage files changed

--- app/src/main/java/com/tushant/gamehok/screens/Homepage.kt
+++ app/src/main/java/com/tushant/gamehok/screens/Homepage.kt
@@ -56,11 +56,9 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.HorizontalPager
 import com.google.accompanist.pager.rememberPagerState
@@ -1133,11 +1131,4 @@ fun SpotlightEffect(
             )
         }
     }
-}
-
-@Preview
-@Composable
-fun HomepagePreview() {
-    val navController = rememberNavController()
-    Homepage(navController = navController)
 }
